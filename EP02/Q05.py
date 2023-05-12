9d472971f76064d53c4bea6de8c0d55e112f1bef
Vitória Maria do Nascimento <116987726+Vitoria-Maria0912@users.noreply.github.com>
2023-05-12 09:59:31 -0300

    Create Q05.py

---
+++ EP02/Q05.py
@@ -0,0 +1,20 @@
+import networkx as nx
+
+
+def change_costs_factor(ddc, c):
+    
+    fator_resultante = 0
+    custo = 1
+
+    # não soma se c depende de c
+    if(outras classes são dependentes de c):
+        fator_resultante += 1
+
+    if(a classe faz parte de um tangle):
+        fator_resultante += 50
+
+    if(a classe faz parte de 1 ou
+        mais ciclos de dependência mínimos):
+        fator_resultante += 10
+
+    return custo + fator_resultante
