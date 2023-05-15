23b59504d8075ba3fe87fb26459b3f138855f9fd
Vitória Maria do Nascimento <116987726+Vitoria-Maria0912@users.noreply.github.com>
2023-05-15 20:35:06 -0300

    Update change_costs_factor.py

--- EP02/Questões/change_costs_factor.py
+++ EP02/Questões/change_costs_factor.py
@@ -2,21 +2,41 @@ import networkx as nx
 
 
 def change_costs_factor(ddc, c):
-    
+
+    # todo grafo tem custo de manutenção 1
+
     fator_resultante = 0
     custo = 1
-    
-    for(dependentes: ddc):
 
-    # não soma se c depende de c
-    if(outras classes são dependentes de c):
+    if(ddc == None or c == None):
+        return custo
+    
+    sem_loop = True
+
+    # verifica se há loop no vértice 'c'
+
+    for node in nx.nodes_with_selfloops(ddc):
+        if(node == c):
+            sem_loop = False
+
+    # não soma se 'c' depende de 'c', ou seja, se 'c' tiver loop
+    # somente se outras classes são dependentes de 'c'
+
+    if(nx.neighborns(ddc, c) != None) and (sem_loop) and (ddc.in_degree(c) > 0):
+
         fator_resultante += 1
 
-    if(a classe faz parte de um tangle):
-        fator_resultante += 50
+        # se há componentes fortes, há um tangle
+        # considera-se apenas tangles > 3  
+        # checar se há componentes fortes do 'node' até alguém ----------------------------------------------
+        if(a classe faz parte de um tangle):
+            fator_resultante += 50
 
-    if(a classe faz parte de 1 ou
-        mais ciclos de dependência mínimos):
-        fator_resultante += 10
+        # o que seria um ciclo de dependência mínima? --------------------------------------------------------
+        if(a classe faz parte de 1 ou
+            mais ciclos de dependência mínimos):
+            fator_resultante += 10
 
-    return custo + fator_resultante
+    custo += fator_resultante
+
+    return custo
