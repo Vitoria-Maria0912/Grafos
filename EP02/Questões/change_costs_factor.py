e447f4891cdcb74f593489a48cfe5820cfc94695
Vitória Maria do Nascimento <116987726+Vitoria-Maria0912@users.noreply.github.com>
2023-05-17 21:03:53 -0300

    Update change_costs_factor.py

--- EP02/Questões/change_costs_factor.py
+++ EP02/Questões/change_costs_factor.py
@@ -3,46 +3,73 @@ import networkx as nx
 
 def change_costs_factor(ddc, c):
 
+    # checa se os parâmetros são válidos e se o grafo é direcionado
+
+    if(ddc == None) or (c == None) or (not nx.is_directed(ddc)) or (c not in ddc.nodes):
+        return None
+    
+    # checa se o grau de entrada do vértice 'c' é igual a zero
+
+    if(ddc.in_degree(c) == 0):
+        return 1
+
     # todo grafo tem custo de manutenção 1
 
     fator_resultante = 0
     custo = 1
-
-    if(ddc == None or c == None):
-        return custo
     
-    sem_loop = True
-    sets = nx.strongly_connected_components
+    sets_tangles = nx.strongly_connected_components(ddc)
+    cycles = minimum_cycle_basis(ddc)
+    neighborhood = ddc.predecessors(c)
 
-    # verifica se há loop no vértice 'c'
-
-    for node in nx.nodes_with_selfloops(ddc):
-        if(node == c):
-            sem_loop = False
-
-    # não soma se 'c' depende de 'c', ou seja, se 'c' tiver loop
+    # não soma se 'c' depende de 'c', ou seja, se a classe tiver loop
     # somente se outras classes são dependentes de 'c'
 
-    if(nx.neighborns(ddc, c) != None) and (sem_loop) and (ddc.in_degree(c) > 0):
+    for neighbor in neighborhood: 
+        if(neighbor != c):
+            fator_resultante += 1
 
-        fator_resultante += 1
+    # se há componentes fortes, há um tangle
+    # considera-se apenas tangles > 3
+    # cada tangle soma 50 ao fator resultante  
+    
+    for set in sets_tangles:
+        if(c in set) and (len(set) > 3):
+            fator_resultante += 50
 
-        # se há componentes fortes, há um tangle
-        # considera-se apenas tangles > 3  
-        # checar se há componentes fortes do 'node' até alguém ----------------------------------------------
-        
-        # a classe faz parte de um tangle
-        for set in sets:
-            if(c in set) and (set.size() > 3):
-                fator_resultante += 50
+    # se a classe faz parte de 1 ou mais ciclos de dependência mínimos
+    # cada ciclo de dependência mínima soma 10 ao fator resultante
 
-        # o que seria um ciclo de dependência mínima? --------------------------------------------------------
-        
-        cycles = nx.simple_cycles(ddc)
-        if(a classe faz parte de 1 ou
-            mais ciclos de dependência mínimos):
+    for cycle in cycles:
+        if(c in cycle) and len(cycle) > 1:
             fator_resultante += 10
 
+    # atualiza o custo somando ao fator resultante   
+    
     custo += fator_resultante
 
     return custo
+
+
+# adptação da função 'nx.minimum_cycle_basis(ddc)'
+# de grafos não direcionados para grafos direcionados
+def minimum_cycle_basis(grafo):
+
+    cycles = nx.simple_cycles(grafo)
+
+    min_cycles = []
+
+    for cycle in cycles:
+
+        is_subcycle = False
+
+        for min_cycle in min_cycles:
+
+            if set(cycle).issubset(set(min_cycle)):
+                is_subcycle = True
+                break
+            
+        if not is_subcycle:
+            min_cycles.append(cycle)
+
+    return min_cycles
