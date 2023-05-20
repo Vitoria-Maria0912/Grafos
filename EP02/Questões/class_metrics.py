28f6b853dce3e35d89fef7e764eeea226dbdcca2
Vitória Maria do Nascimento <116987726+Vitoria-Maria0912@users.noreply.github.com>
2023-05-19 23:05:37 -0300

    Update class_metrics.py

--- EP02/Questões/class_metrics.py
+++ EP02/Questões/class_metrics.py
@@ -2,11 +2,15 @@ import networkx as nx
 
 # Author: Pedro Henrique Sarmento Pereira
 
+
 def class_metrics(ddc, c):
+
+    # verifica se os parâmetros são válidos e se o grafo é direcionadp.
     
-    # Checa se os parâmetros são válidos e se o grafo é direcionado
-    
-    if(ddc is None) or (c is None) or (c not in ddc) or not(nx.is_directed(ddc)):
+    if(ddc is None or c is None or c not in ddc or not nx.is_directed(ddc)):
         return None
+    
+    # retorna uma tupla com o número de vértices que o vértice 'c', pertencente a ddc, depende.
+    # e o número de vértices que dependem do vértice 'c', pertencente a ddc.
     
     return (ddc.out_degree(c),ddc.in_degree(c))
